0f91d2cfcaeb24ab73ef0b7f0a5c4ef6f7330896
MosesLevin <moses.levin14@gmail.com>
2024-03-05 16:11:19 +0100

    removed unnecessary components, added Loading & Message for error, cors middleware for express, cors@types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express'
 import { sampleProducts } from './data'
+import cors from 'cors'
 
+//cors middleware to accept requests from 5173
 const app = express()
+app.use(cors({ credentials: true, origin: ['http://localhost:5173'] }))
 const port = 3000
 
 app.get('/api/products', (req, res) => {
