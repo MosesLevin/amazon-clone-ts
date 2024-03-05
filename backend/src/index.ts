9919c08fd5c0cec19db43160939fcd3adff96ca1
MosesLevin <moses.levin14@gmail.com>
2024-03-05 14:10:30 +0100

    added build, dev scripts in package.json, express route to sampleProducts data in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,13 @@
 import express, { Request, Response } from 'express'
 import { sampleProducts } from './data'
+
+const app = express()
+const port = 3000
+
+app.get('/api/products', (req, res) => {
+  res.json(sampleProducts)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
